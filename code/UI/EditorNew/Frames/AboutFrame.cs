fce2c2f9d43d2401ba3c10888db064aa8c7d30fb
BagraToGovnoKoder <94111299+BagraToGovnoKoder@users.noreply.github.com>
2021-12-23 21:50:30 +0200

    ssss

--- code/UI/EditorNew/Frames/AboutFrame.cs
+++ code/UI/EditorNew/Frames/AboutFrame.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TerryNovel.Editor;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
 
 namespace TerryNovel
 {
 	public class AboutFrame:Frame
 	{
-
+        public AboutFrame()
+		{
+			Canvas.Add.Label("About us: we are three friends from russia", "about");
+		}
 	}
 }
