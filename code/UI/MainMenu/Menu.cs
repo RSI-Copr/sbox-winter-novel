bf84c6b81d227c9508bb8e617aacbe84abf6f289
nikita-strelok <romayakushev1979@gmail.com>
2021-12-23 19:30:02 +0200

    Scroll

--- code/UI/MainMenu/Menu.cs
+++ code/UI/MainMenu/Menu.cs
@@ -1,23 +1,50 @@
 ﻿using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
+using Sandbox.UI.Tests;
 using TerryNovel.Editor;
 
 namespace TerryNovel
 {
 	[UseTemplate]
-	public class MainMenu:Panel
+	public class MainMenu : Panel
 	{
+		private VirtualScrollPanel NovelsList;
 		public MainMenu()
 		{
-			
+			NovelsList = AddChild<VirtualScrollPanel>( "novels-list" );
+			NovelsList.Layout.ItemHeight = Length.Percent( 50f ).Value;
+			NovelsList.OnCreateCell = OnCellCreated;
 
-			
+
 		}
+
+		private void OnCellCreated( Panel cell, object data )
+		{
+			cell.AddChild<NovelEntry>();
+		}
+
+
+		public class NovelEntry:Panel{
+			public NovelEntry()
+			{
+				AddClass( "entry" );
+
+				var pnl = Add.Panel( "labels" );
+
+				pnl.Add.Label( "Test", "title" );
+				pnl.Add.Label( "Description", "desc" );
+
+				Add.Panel( "img-canvas" ).Add.Image( "UI/background-grid.png" );
+			}
+		}
+
 
 		public void Startnovel()
 		{
+			NovelsList.SetVisible( true );
 
+			NovelsList.AddItem( "test" );
 		}
 
 		public void ShowAbout()
@@ -33,5 +60,13 @@ namespace TerryNovel
 		{
 			Local.Client.Kick();
 		}
+
+
+
+
 	}
+
+
+
 }
+
