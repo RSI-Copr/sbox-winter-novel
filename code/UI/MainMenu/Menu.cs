c0fd053a1f2c0c4b0d99e786a196449a4e5f90bc
nikita-strelok <romayakushev1979@gmail.com>
2021-12-23 23:02:06 +0200

    Menu

--- code/UI/MainMenu/Menu.cs
+++ code/UI/MainMenu/Menu.cs
@@ -11,6 +11,7 @@ namespace TerryNovel
 	{
 
 		private NovelCanvas NovelCanvas;
+		public Panel Snow { get; set; }
 
 		public MainMenu()
 		{
@@ -21,6 +22,7 @@ namespace TerryNovel
 		public void Startnovel()
 		{
 			NovelCanvas.SetVisible(true);
+			NovelCanvas.Update();
 		}
 
 		public void ShowAbout()
@@ -36,57 +38,98 @@ namespace TerryNovel
 		{
 			Local.Client.Kick();
 		}
+		private float x_offset = 0;
+		public override void Tick()
+		{
+			//Snow.Style.BackgroundPositionX = x_offset;
+			x_offset++;
+			
+		}
 	}
 
-	public class NovelCanvas: Panel
+	public class NovelCanvas : Panel
 	{
-		
-		private Panel NovelsList;
+
+		private Panel List;
 		private Button StartButtton;
+
+
+
 		public NovelCanvas()
 		{
-			AddClass("novels-canvas");
-
-
-
-
-			NovelsList = Add.Panel( "novels-list" );
-
-			NovelsList.Add.Panel("category").Add.Label("Main");/////
-
-			NovelsList.AddChild<NovelEntry>();
-			NovelsList.AddChild<NovelEntry>();
-
-			NovelsList.Add.Panel( "category" ).Add.Label( "Bonus" );/////
-
-			NovelsList.AddChild<NovelEntry>();
-
-			var btns = Add.Panel("buttons");
+			AddClass( "novels-canvas" );
+			List = Add.Panel( "novels-list" );
 			
-			var back = btns.Add.Button( "Back","back" );
+			var btns = Add.Panel( "buttons" );
+
+
+			
+			var back = btns.Add.Button( "Back", "back", ()=> this.SetVisible(false) );
 			StartButtton = btns.Add.Button( "Start", "start" );
 
 		}
 
-		private void OnCellCreated(Panel cell, object data)
+		public void AddCategory(string name)
 		{
-			cell.AddChild<NovelEntry>();
+			List.Add.Panel( "category" ).Add.Label( name );
+		}
+
+		public void Update()
+		{
+			List.DeleteChildren( true );
+			AddCategory( "Main" );
+			FindFrom( FileSystem.Mounted, "base_novels/main/" );
+			AddCategory( "Bonus" );
+			FindFrom( FileSystem.Mounted, "base_novels/bonus/" );
+			AddCategory( "Custom" );
+			FindFrom( FileSystem.Data, "novels/" );
+
+		}
+
+
+		
+
+		public void FindFrom(BaseFileSystem fileSystem, string dir )
+		{
+			foreach(var file in Novel.FindAll( fileSystem, dir ) )
+			{
+				if( Novel.ReadInfo( fileSystem, file, out var name, out var desc ) )
+				{
+					var entry = new NovelEntry( fileSystem, dir, name, desc );
+					entry.AddEventListener( "onclick", () => Novel.LoadFromFile( fileSystem, file ) );
+					List.AddChild( entry );
+				}
+			}
 		}
 
 
 		public class NovelEntry : Panel
 		{
-			public NovelEntry()
+			public NovelEntry( BaseFileSystem fileSystem, string dir, string name, string desc)
 			{
 				AddClass("entry");
 
 				var pnl = Add.Panel("labels");
 
-				pnl.Add.Label("Test", "title");
-				pnl.Add.Label("Description", "desc");
+				pnl.Add.Label( name, "title");
+				pnl.Add.Label( desc, "desc");
 
-				Add.Panel("img-canvas").Add.Image("UI/background-grid.png");
+				var f_name = $"{dir}/preview.png";
+
+				var img_canvas = Add.Panel( "img-canvas" );
+
+				if (fileSystem == FileSystem.Mounted && fileSystem.FileExists( name ) )
+				{
+					img_canvas.Add.Image( f_name );
+				}
+				else
+				{
+					img_canvas.Add.Image( "UI/background-grid.png" );
+				}
+
+				
 			}
+
 		}
 
 	}
